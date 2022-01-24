1ac3eec60e6b60b9e3e66341550271f75cfc114f
Wonder2018 <49940418+Wonder2018@users.noreply.github.com>
2022-01-24 10:34:17 +0800

    修正错误的按键索引
    Signed-off-by: Wonder2018 <49940418+Wonder2018@users.noreply.github.com>

--- src/top/imwonder/musicongenshin/pojo/LyreToneEnum.java
+++ src/top/imwonder/musicongenshin/pojo/LyreToneEnum.java
@@ -4,9 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum LyreToneEnum {
+    LDO("-1", 'Z'), LRE("-2", 'X'), LMI("-3", 'C'), LFA("-4", 'V'), LSOL("-5", 'B'), LLA("-6", 'N'), LSI("-7", 'M'),
+    DO("1", 'A'), RE("2", 'S'), MI("3", 'D'), FA("4", 'F'), SOL("5", 'G'), LA("6", 'H'), SI("7", 'J'),
     HDO("+1", 'Q'), HRE("+2", 'W'), HMI("+3", 'E'), HFA("+4", 'R'), HSOL("+5", 'T'), HLA("+6", 'Y'), HSI("+7", 'U'),
-    DO("1", 'A'), RE("2", 'S'), MI("3", 'D'), FA("4", 'F'), SOL("5", 'G'), LA("6", 'H'), SI("7", 'J'), LDO("-1", 'Z'),
-    LRE("-2", 'X'), LMI("-3", 'C'), LFA("-4", 'V'), LSOL("-5", 'B'), LLA("-6", 'N'), LSI("-7", 'M'), STOP("0", 'P');
+    STOP("0", 'P');
 
     private String record;
 
